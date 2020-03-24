08d6289c21f4cba50b2bb5dc9a09da96369636cc
Maria Burlaka <mariaburlaka.94@gmail.com>
2020-03-24 19:36:46 +0300

    Update SDK

--- DevinoDemo/ViewController.swift
+++ DevinoDemo/ViewController.swift
@@ -86,7 +86,12 @@ class ViewController: UIViewController {
         Devino.shared.sendCurrentSubscriptionStatus(isSubscribe: Devino.isUserNotificationsAvailable)// from settings - Devino.isUserNotificationsAvailable
         
         Devino.shared.getLastSubscriptionStatus { result in
-            print("LastSubscriptionStatus = \(result)")
+            switch result {
+            case .success(let result):
+                print("Success LastSubscriptionStatus = \(result)")
+            case .failure(let error):
+                print("Error LastSubscriptionStatus = \(error.localizedDescription)")
+            }
         }
     }
     
