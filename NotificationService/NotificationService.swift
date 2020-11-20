074276d96238cb46716b2c777642fd8c038efd68
Maria Burlaka <mvburlaka@gmail.com>
2020-11-20 15:35:47 +0300

    Update project

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -7,5 +7,31 @@
 //
 
 import DevinoSDK
+import UserNotifications
 
-class NotificationService: DevinoNotificationService {}
+class NotificationService: DevinoNotificationService {
+    
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let actionIdentifier = response.actionIdentifier
+        print("ACTION ID = \(actionIdentifier)")
+        /// Identify the action by matching its identifier.
+        switch actionIdentifier {
+        case UNNotificationDefaultActionIdentifier:
+            Devino.shared.trackNotificationResponse(response)
+            if let actionButton = response.notification.request.content.userInfo["action"] as? [String: String], let action = actionButton["action"] {
+            }
+            print("Action default")
+        case UNNotificationDismissActionIdentifier:
+            Devino.shared.trackNotificationResponse(response, actionIdentifier)
+            print("Action dismiss")
+        default:
+            Devino.shared.trackNotificationResponse(response, actionIdentifier)
+            print("Action = \(actionIdentifier)")
+        }
+        completionHandler()
+    }
+}
